74774a7bc1fef03c7eab405fead8ae5ea6880ae9
Rickard Pettersson <rickard971@hotmail.com>
2023-12-11 13:57:51 +0100

    Added early version how Observer Pattern would be integrated

--- src/API.java
+++ src/API.java
@@ -59,3 +59,44 @@ public class API {
         return jsonobj;
     }
 }
+
+
+
+/*
+
+APIConnection{
+    private <T> dataFromAPI;
+    Private List <Subscriber> listOfObservers = new ArrayList<>();
+
+    Public void addObserver(Subscriber subscriber){
+        this.listOfObservers.add(subscriber);
+    }
+
+    public void removeObserver(Subscriber subscriber){
+        this.listOfObservers.remove(subscriber);
+    }
+
+    public void notify(<T> dataFromAPI){
+        this.dataFromAPI = dataFromAPI;
+        for(Subscriber subscriber : listOfObservers){
+            subscriber.update(this.dataFromAPI);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+
+
+
+
---
+++ src/ExchangeDataVisualizer.java
@@ -0,0 +1,11 @@
+public class ExchangeDataVisualizer implements Subscriber{
+
+    private dataFromAPI;
+
+    @Override
+    public void update(Object dataFromAPI){
+        this.dataFromAPI(<T> dataFromAPI)
+        Kod
+        repaint();
+    }
+}
