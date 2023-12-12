57094b0a223174d429a04b95c6bd3a6b442b2114
Filip <fillip.detterfelt@gmail.com>
2023-12-12 12:48:53 +0100

    Extend jframe and build more on the method, added a main method for testing

--- src/Gui.java
+++ src/Gui.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Gui {
+public class Gui extends JFrame{
     //Huvudpanel
     JPanel headPanel = new JPanel();
     JPanel topPanel = new JPanel(); //Huvudpanelen för appen
@@ -47,5 +47,95 @@ public class Gui {
 
         centerPanel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.insets = new Insets(2,5,5,5); //Marginal mellan komponenter
+
+        //Lägger till frånlabel och frånvalutaComboBox i centrum
+        //Fixa så att de flyttas åt höger respektive vänster
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        centerPanel.add(frånLabel,gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        centerPanel.add(frånValutaComboBox,gbc);
+        gbc.gridx = 1;
+        frånText.setColumns(5);
+        centerPanel.add(frånText,gbc);
+
+
+        //Lägger till tillLabel och tillValutaComboBox i centrum
+
+        gbc.gridx = 2; //Öka gridX för att flytta den mer åt höger
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        centerPanel.add(tillLabel, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        centerPanel.add(tillValutaComboBox, gbc);
+        gbc.gridx = 3; //Flyttar gridx med 1 så att den hamnar till höger om combobox
+        tillText.setColumns(5); //Ändrar bredden på textfield
+        centerPanel.add(tillText,gbc);
+
+
+
+        //Ändra storlek på convert knappen
+        int convertwidth = 200;
+        int convertheigth = 40;
+        Dimension newDimensionConvertButton = new Dimension(convertwidth, convertheigth);
+        convertButton.setPreferredSize(newDimensionConvertButton);
+
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        centerPanel.add(convertButton, gbc);
+
+
+
+        //Bottompanel
+        gbc.insets = new Insets(5,5,5,5);
+        gbc.gridx =0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        bottomPanel.add(actualCurrencySek,gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        bottomPanel.add(sourceOfData,gbc);
+
+
+
+
+
+
+
+
+       /* centerPanel.add(frånLabel);
+        centerPanel.add(frånValutaComboBox);
+
+        centerPanel.add(tillLabel);
+        centerPanel.add(tillValutaComboBox);*/
+
+
+        //Lägg till bilden på granen här
+        setTitle("The X:changer");
+        setSize(400,650);
+        Color myColor = Color.decode("#6c25be");
+        topPanel.setBackground(myColor);
+        centerPanel.setBackground(myColor);
+        bottomPanel.setBackground(myColor);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setResizable(false);
+
+    }
+
+    public static void main(String[] args) {
+        Gui g = new Gui();
+        g.drawXchangePanel();
+    }
 
 }
