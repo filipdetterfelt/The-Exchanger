7cd793e5c1134de9ebe6a0e914483ff5a2f8428d
Maria König <maria.konig@yh.nackademin.se>
2023-12-14 00:05:24 +0100

    add isValidInput

--- src/GUIController.java
+++ src/GUIController.java
@@ -1,34 +1,44 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUIController implements ActionListener , Subscriber {
+public class GUIController implements ActionListener, Subscriber {
 
     GUI gui;
     API api;
 
-GUIController(API api, GUI gui){
-    this.api = api;
-    this.gui = gui;
+    String originalAmount= "text";          //Måste finnas bokstäver här pga null-värde
 
-    gui.getConvertButton().addActionListener(this);
-}
+    GUIController(API api, GUI gui) {
+        this.api = api;
+        this.gui = gui;
+
+        gui.getConvertButton().addActionListener(this);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == gui.getConvertButton()) {
-        api.setApiExchangeInput(Currencies.SEK, Currencies.NOK, 200);
-    }
+        if (e.getSource() == gui.getConvertButton()) {
+            if(isValidInput(originalAmount)) {
+                api.setApiExchangeInput(Currencies.SEK, Currencies.NOK, 200);
+            }
         }
+    }
 
     @Override
     public void update(ExchangeInfo info) {
         gui.updateExchangedAmount(info.getExchangedAmount());
-        gui.updateRateInformation(info.getBaseCurrency(),info.getTargetCurrency(), info.getRate(), info.getReverseRate());
+        gui.updateRateInformation(info.getBaseCurrency(), info.getTargetCurrency(), info.getRate(), info.getReverseRate());
         gui.updateExchangedDate(info.getDate());
 
     }
+
+    public boolean isValidInput(String amount) {
+        return amount.matches("[0-9]+");
+    }
 }
-    //Actionlistener
+
+
+//Actionlistener
 /*
     ActionListener (Convert){
         api.setApiExchangeInput(view.getCurrencyFrom(), view.getCurrencyTo(), view.getAmountFrom());
