7a739213ca545981cad667f6fc067dad200b7247
Maria König <maria.konig@yh.nackademin.se>
2023-12-13 01:26:37 +0100

    Refactored code

--- src/API.java
+++ src/API.java
@@ -2,24 +2,19 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.InputStreamReader;
-import java.io.InputStream;
 import java.io.BufferedReader;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-public class API implements Observed{
+public class API implements Observed {
     private static final String API_KEY = "c419a2f16c0967ee3eaa58f1";
     private static final String API_URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/";
-    private ExchangeInfo info;
+    private final ArrayList<Subscriber> listOfObservers = new ArrayList<>();
 
-    private ArrayList<Subscriber> listOfObservers = new ArrayList<>();
     public void setApiExchangeInput(Enum<Currencies> baseCurrency, Enum<Currencies> targetCurrencyEnum, double amount) {
-        double exhangedAmount = 0;
+
         try {
             String targetCurrency = targetCurrencyEnum.toString();
             URL url = new URL(API_URL + baseCurrency);
@@ -28,18 +23,16 @@ public class API implements Observed{
             request.setRequestMethod("GET");
             request.connect();
 
-            JsonObject jsonobj = getJsonObject(request);
-
-            JsonObject rates = jsonobj.getAsJsonObject("conversion_rates");
+            JsonObject jsonObj = getJsonObject(request);
+            JsonObject rates = jsonObj.getAsJsonObject("conversion_rates");
             double rate = rates.get(targetCurrency).getAsDouble();
-
             double exchangedAmount = rate * amount;
 
-            info = new ExchangeInfo(exchangedAmount, rate,baseCurrency.toString(), targetCurrency, jsonobj.get("time_last_update_unix").getAsString());
+            ExchangeInfo info = new ExchangeInfo(exchangedAmount, rate, baseCurrency.toString(), targetCurrency, jsonObj.get("time_last_update_unix").getAsString());
             notifySubscriber(info);
 
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(System.out);
         }
     }
     /*public Map<String, ExchangeInfo> currencyExtractedInfoMap(ExchangeInfo info) {
@@ -53,9 +46,12 @@ public class API implements Observed{
     }*/
 
     private static JsonObject getJsonObject(HttpURLConnection request) throws IOException {
-        InputStream inputStream = request.getInputStream();
+        /*InputStream inputStream = request.getInputStream();
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader reader = new BufferedReader(inputStreamReader);
+        */
+        ////Skrev om bort kommenterade rader över till undre rad
+        BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
 
         // Konvertera svaret till en sträng
         StringBuilder response = new StringBuilder();
@@ -66,12 +62,14 @@ public class API implements Observed{
         reader.close();
 
         // Använd GSON-biblioteket för att konvertera strängen till ett JSON-objekt
-        JsonParser parser = new JsonParser();
+        /*JsonParser parser = new JsonParser();
         JsonElement root = parser.parse(response.toString());
         JsonObject jsonobj = root.getAsJsonObject();
-        return jsonobj;
+        return jsonobj;*/
+        //Skrev om bort kommenterade rader över till undre rader
+        JsonElement root = JsonParser.parseString(response.toString());
+        return root.getAsJsonObject();
     }
-
 
     @Override
     public void addSubscriber(Subscriber subscriber) {
@@ -80,7 +78,7 @@ public class API implements Observed{
 
     @Override
     public void removeSubscriber(Subscriber subscriber) {
-        this.listOfObservers.remove(subscriber);
+        this.listOfObservers.remove( subscriber);
     }
 
     @Override
