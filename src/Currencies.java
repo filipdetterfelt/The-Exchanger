8075604d58f6b9c9b03a34ef6252f762428ab324
Alexander <146874712+AlexanderBorggren@users.noreply.github.com>
2023-12-14 19:58:59 +0100

    More currencies

--- src/Currencies.java
+++ src/Currencies.java
@@ -1,3 +1,4 @@
+import java.security.KeyStore;
 
 public enum Currencies {
 
@@ -6,12 +7,33 @@ public enum Currencies {
     EUR ("Euro"),
     SEK ("Svensk Krona"),
     NOK ("Norsk Krona"),
-    DKK ("Dansk Krona");
+    DKK ("Dansk Krona"),
+    RUB ("Russian ruble"),
+    INR ("Indian rupee"),
+    CNY ("Chinese yuan"),
+    JPY ("Japanese yen"),
+    KRW ("South Korean won"),
+    IDR ("Indonesian rupiah"),
+    THB ("Thai baht"),
+    MYR ("Malaysian ringgit"),
+    VND ("Vietnamese dong"),
+    BRL ("Brazilian real"),
+    ZAR ("South African rand"),
+    CAD ("Canadian dollar"),
+    AUD ("Australian dollar"),
+    NZD ("New Zealand dollar"),
+    HKD ("Hong Kong dollar"),
+    SGD ("Singapore dollar"),
+    CHF ("Swiss franc"),
+    MXN ("Mexican peso"),
+    PHP ("Philippine peso"),
+    PLN ("Polish zloty"),
+    TRY ("Turkish lira");
+
 
     public final String fullName;
 
      Currencies(String s){
          fullName = s;
      }
-
 }
